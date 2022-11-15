9533b38628781687188e0f25e076f2055a5c8a7c
mehdi-romdhani <mehdi.romdhani@laplateforme.io>
2022-11-15 11:51:56 +0100

    boucle for

---
+++ jour02/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+for($i = 0; $i <= 1337; $i++){
+    
+    if($i === 42 ){
+
+        echo " ==========> <b><u>le numero $i est en gras </u></b>";
+        echo "<br>" ;
+    }
+    else{
+
+        echo "le nombre est $i" ;
+        echo"<br>"; 
+        
+    }
+    
+
+}
+
+
+
