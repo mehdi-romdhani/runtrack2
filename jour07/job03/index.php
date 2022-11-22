18f863399fe8a2ab22fdcbe66762aa82e43c1bd1
mehdi-romdhani <mehdi.romdhani@laplateforme.io>
2022-11-22 10:39:29 +0100

    retour d'un resultat

---
+++ jour07/job03/index.php
@@ -0,0 +1,19 @@
+<?php
+
+//Declaration function with argument 
+
+//Ecriture de la fonction en camelCase
+
+function getHello(){
+
+    
+    return "Hello _ la _ Plateforme";
+
+   
+
+};
+
+//Apelle de la function ;
+
+echo getHello();
+
