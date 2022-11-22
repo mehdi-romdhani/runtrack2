76eea56a088313624b3f719d13603015143c465f
mehdi-romdhani <mehdi.romdhani@laplateforme.io>
2022-11-22 10:18:35 +0100

    form_style

---
+++ jour06/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form_style</title>
+</head>
+<body>
+    <form action="#" method="get">
+        <caption>Formulaire Style</caption>
+        <thead>
+            
+        </thead>
+    </form>
+</body>
+</html>
